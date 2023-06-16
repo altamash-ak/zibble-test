7be9f76254db0cac83218b90b93c7f8686fce8d3
altamash-nxt <altamash360@gmail.com>
2023-06-16 20:45:57 +0500

    fix jquery issue

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
-$(document).ready(function () {
-  $(".awards-slider").slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    arrow: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"></button>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
+$(".awards-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  arrow: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-    ],
-  });
+    },
+  ],
 });
